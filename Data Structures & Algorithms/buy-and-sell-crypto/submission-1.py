bd17b4149967b96870f869d9764068d9a3eefd9f
vegeta-666 <wayne19891106@gmail.com>
2026-09-11 09:51:23 +0800

    Add: buy-and-sell-crypto - submission-1

---
+++ Data Structures & Algorithms/buy-and-sell-crypto/submission-1.py
@@ -0,0 +1,15 @@
+class Solution:
+    def maxProfit(self, prices: List[int]) -> int:
+        # 記錄當前最大獲利
+        # 紀錄最小值
+        res = 0
+        min = prices[0]
+        for price in prices:
+            # print(price)
+            if price < min:
+                min = price
+                continue
+            else:
+                res = max(res, price-min)
+
+        return res
